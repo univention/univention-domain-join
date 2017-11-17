c94a6caa1c0c532456e03c478089384a1d84f62e
Richard Ulmer <ulmer@univention.de>
2017-11-17 10:36:57 +0100

    Bug #35981: Improved extensibility for new distributions

--- cli.py
+++ cli.py
@@ -1,116 +1,71 @@
+from getpass import getpass
 import argparse
-import getpass
+import importlib
 import os
 import subprocess
-
-from join_steps.kerberos_configurator import KerberosConfigurator
-from join_steps.ldap_configurator import LdapConfigurator
-from join_steps.login_manager_configurator import LoginManagerConfigurator
-from join_steps.pam_configurator import PamConfigurator
-from join_steps.sssd_configurator import SssdConfigurator
 
 OUTPUT_SINK = open(os.devnull, 'w')
 
 # TODO: Make sure dependent packets are installed in the Debian package.
+# TODO: Back up (with timestamp) all files that will be touched into /var/univention/backup .
+#       Leaving out any old LDAP objects.
+# TODO: Check for any conflicts beforehand (checks) and abort if there is any.
 
 
-class UcsJoiner(object):
-	def __init__(self, master_ip):
-		self.check_if_this_is_run_as_root()
-		self.check_if_dns_is_set_up_correctly(master_ip)
-		self.master_pw = self.get_masters_root_password(master_ip)
+def check_if_dns_is_set_up_correctly(master_ip):
+	# TODO: Is 'host' usable across distributions?
+	master_dns_works = 0 == subprocess.call(
+		['host', master_ip], stdout=OUTPUT_SINK, stderr=OUTPUT_SINK
+	)
+	assert master_dns_works, 'The DC master is not set as DNS server.'
 
-		masters_ucr_variables = self.get_ucr_variables_from_master(master_ip, self.master_pw)
 
-		self.master_ip = master_ip
-		self.ldap_master = masters_ucr_variables['ldap_master']
-		self.ldap_base = masters_ucr_variables['ldap_base']
-		self.kerberos_realm = masters_ucr_variables['kerberos_realm']
+def get_joiner_for_this_distribution(master_ip):
+	master_pw = get_masters_root_password(master_ip)
+	masters_ucr_variables = get_ucr_variables_from_master(master_ip, master_pw)
 
-	def check_if_this_is_run_as_root(self):
-		assert os.geteuid() == 0, 'This tool must be run as the root user.'
+	distribution = get_distribution()
+	try:
+		distribution_join_module = importlib.import_module('distributions.%s' % (distribution.lower(),))
+	except ImportError:
+		raise Exception('The used distribution "%s" is not supported.' % (distribution,))
+	return distribution_join_module.Joiner(
+		masters_ucr_variables, master_ip, master_pw
+	)
 
-	def check_if_dns_is_set_up_correctly(self, master_ip):
-		master_dns_works = 0 == subprocess.call(
-			['host', master_ip], stdout=OUTPUT_SINK, stderr=OUTPUT_SINK
-		)
-		assert master_dns_works, 'The DC master is not set as DNS server.'
 
-	def get_masters_root_password(self, master_ip):
-		return getpass(prompt='Please enter the password for root@%s:' % (master_ip,))
-		# TODO: Check if password works.
+def get_distribution():
+	return subprocess.check_output(['lsb_release', '-is']).strip()
 
-	def get_ucr_variables_from_master(self, master_ip, master_pw):
-		masters_ucr_output = subprocess.check_output(
-			['ssh', 'root@%s' % (master_ip,), 'ucr shell | grep -v ^hostname='],
-		).splitlines()
-		ucr_variables = {}
-		for raw_ucr_variable in masters_ucr_output:
-			key, value = raw_ucr_variable.split('=', 1)
-			ucr_variables[key] = value
-		return ucr_variables
 
-	def join_domain(self, force=False):
-		self.configure_ldap(force)
-		self.configure_sssd(force)
-		self.configure_pam(force)
-		self.configure_login(force)
-		self.configure_kerberos(force)
-		# TODO: Stop avahi service like Jan-Christoph does?!
+def get_masters_root_password(master_ip):
+	return getpass(prompt='Please enter the password for root@%s: ' % (master_ip,))
+	# TODO: Check if password works.
 
-	def configure_ldap(self, force=False):
-		if force or not LdapConfigurator().ldap_configured(self.master_ip, self.ldap_base):
-			LdapConfigurator().configure_ldap(self.master_ip, self.ldap_master, self.ldap_base)
-		else:
-			print('The LDAP seems to be configured already. Skipping this step.')
 
-	def configure_sssd(self, force=False):
-		if force or not SssdConfigurator().sssd_configured():
-			SssdConfigurator().setup_sssd(self.master_ip, self.ldap_master, self.ldap_base, self.kerberos_realm)
-		else:
-			print('sssd seems to be configured already. Skipping this step.')
-
-	def configure_pam(self, force=False):
-		if force or not PamConfigurator().pam_configured():
-			PamConfigurator().setup_pam()
-		else:
-			print('PAM seems to be configured already. Skipping this step.')
-
-	def configure_login(self, force=False):
-		if force or not LoginManagerConfigurator().login_manager_configured():
-			if LoginManagerConfigurator().login_manager_compatible():
-				LoginManagerConfigurator().enable_login_with_foreign_usernames()
-			else:
-				print('Warning: The login manager remains unconfigured.')
-		else:
-			print('PAM seems to be configured already. Skipping this step.')
-
-	def configure_kerberos(self, force=False):
-		if force or not KerberosConfigurator().kerberos_configured(self.ldap_master):
-			KerberosConfigurator().configure_kerberos(self.kerberos_realm, self.master_ip, self.ldap_master)
-		else:
-			print('Kerberos seems to be configured already. Skipping this step.')
-
-	def show_join_status(self):
-		if (
-			LdapConfigurator().ldap_configured(self.master_ip, self.ldap_base) and
-			SssdConfigurator().sssd_configured() and
-			PamConfigurator().pam_configured() and
-			LoginManagerConfigurator().login_manager_configured() and
-			KerberosConfigurator().kerberos_configured(self.ldap_master)
-		):
-			print('This client has been joined into your UCS domain.')
-		else:
-			print('This client has not fully joined an UCS domain.')
+def get_ucr_variables_from_master(master_ip, master_pw):
+	masters_ucr_output = subprocess.check_output(
+		['ssh', 'root@%s' % (master_ip,), 'ucr shell | grep -v ^hostname='],
+	).splitlines()
+	ucr_variables = {}
+	for raw_ucr_variable in masters_ucr_output:
+		key, value = raw_ucr_variable.split('=', 1)
+		ucr_variables[key] = value
+	return ucr_variables
 
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(
-		description='Tool for checking the join status and execute a join of an Ubuntu client in an UCS domain.'
+		description='Tool for joining an Ubuntu client into an UCS domain.'
 	)
-	parser.add_argument('--join', action='store_true', help='Join an UCS domain.')
-	parser.add_argument('--force', action='store_true', help='Force the execution of the join steps, even if they seem to have been executed before.')
-	parser.add_argument('MASTER_IP', help='IP of the DC master.')
+	parser.add_argument('--force', action='store_true', help='Force the execution of the join steps, even if this overwrites configuration files.')
+	parser.add_argument('master_ip', help='IP of the DC master.')
+	args = parser.parse_args()
 
-	ucs_joiner = UcsJoiner('10.200.36.30')
-	ucs_joiner.join_domain()
+	check_if_dns_is_set_up_correctly(args.master_ip)
+	distribution_joiner = get_joiner_for_this_distribution(args.master_ip)
+
+	if not args.force:
+		distribution_joiner.check_if_join_is_possible_without_problems()
+	distribution_joiner.create_backup_of_config_files()
+	distribution_joiner.join_domain()
---
+++ distributions/ubuntu.py
@@ -0,0 +1,80 @@
+import os
+
+from join_steps.kerberos_configurator import KerberosConfigurator
+from join_steps.ldap_configurator import LdapConfigurator
+from join_steps.login_manager_configurator import LoginManagerConfigurator
+from join_steps.pam_configurator import PamConfigurator
+from join_steps.sssd_configurator import SssdConfigurator
+
+
+class Joiner(object):
+	def __init__(self, masters_ucr_variables, master_ip, master_pw):
+		self.check_if_this_is_run_as_root()
+
+		self.master_ip = master_ip
+		self.master_pw = master_pw
+		self.ldap_master = masters_ucr_variables['ldap_master']
+		self.ldap_base = masters_ucr_variables['ldap_base']
+		self.kerberos_realm = masters_ucr_variables['kerberos_realm']
+
+	def check_if_this_is_run_as_root(self):
+		assert os.geteuid() == 0, 'This tool must be run as the root user.'
+
+	def check_if_join_is_possible_without_problems(self):
+		pass
+
+	def create_backup_of_config_files(self):
+		pass
+
+	def join_domain(self, force=False):
+		self.configure_ldap(force)
+		self.configure_sssd(force)
+		self.configure_pam(force)
+		self.configure_login(force)
+		self.configure_kerberos(force)
+		# TODO: Stop avahi service like Jan-Christoph does?!
+
+	def configure_ldap(self, force=False):
+		if force or not LdapConfigurator().ldap_configured(self.master_ip, self.ldap_base):
+			LdapConfigurator().configure_ldap(self.master_ip, self.ldap_master, self.ldap_base)
+		else:
+			print('The LDAP seems to be configured already. Skipping this step.')
+
+	def configure_sssd(self, force=False):
+		if force or not SssdConfigurator().sssd_configured():
+			SssdConfigurator().setup_sssd(self.master_ip, self.ldap_master, self.ldap_base, self.kerberos_realm)
+		else:
+			print('sssd seems to be configured already. Skipping this step.')
+
+	def configure_pam(self, force=False):
+		if force or not PamConfigurator().pam_configured():
+			PamConfigurator().setup_pam()
+		else:
+			print('PAM seems to be configured already. Skipping this step.')
+
+	def configure_login(self, force=False):
+		if force or not LoginManagerConfigurator().login_manager_configured():
+			if LoginManagerConfigurator().login_manager_compatible():
+				LoginManagerConfigurator().enable_login_with_foreign_usernames()
+			else:
+				print('Warning: The login manager remains unconfigured.')
+		else:
+			print('PAM seems to be configured already. Skipping this step.')
+
+	def configure_kerberos(self, force=False):
+		if force or not KerberosConfigurator().kerberos_configured(self.ldap_master):
+			KerberosConfigurator().configure_kerberos(self.kerberos_realm, self.master_ip, self.ldap_master)
+		else:
+			print('Kerberos seems to be configured already. Skipping this step.')
+
+	def show_join_status(self):
+		if (
+			LdapConfigurator().ldap_configured(self.master_ip, self.ldap_base) and
+			SssdConfigurator().sssd_configured() and
+			PamConfigurator().pam_configured() and
+			LoginManagerConfigurator().login_manager_configured() and
+			KerberosConfigurator().kerberos_configured(self.ldap_master)
+		):
+			print('This client has been joined into your UCS domain.')
+		else:
+			print('This client has not fully joined an UCS domain.')

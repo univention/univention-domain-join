8d6fe8d799cfaa515fae3880e2d3e94aaf53fed8
Richard Ulmer <ulmer@univention.de>
2018-02-22 16:31:33 +0100

    Bug #35981: Add missing license notes

--- univention_domain_join/utils/domain.py
+++ univention_domain_join/utils/domain.py
@@ -1,3 +1,34 @@
+#!/usr/bin/env python
+#
+# Univention Domain Join
+#
+# Copyright 2017-2018 Univention GmbH
+#
+# http://www.univention.de/
+#
+# All rights reserved.
+#
+# The source code of this program is made available
+# under the terms of the GNU Affero General Public License version 3
+# (GNU AGPL V3) as published by the Free Software Foundation.
+#
+# Binary versions of this program provided by Univention to you as
+# well as other copyrighted, protected or trademarked materials like
+# Logos, graphics, fonts, specific documentations and configurations,
+# cryptographic keys etc. are subject to a license agreement between
+# you and Univention and not subject to the GNU AGPL V3.
+#
+# In the case you use this program under the terms of the GNU AGPL V3,
+# the program is provided in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public
+# License with the Debian GNU/Linux or Univention distribution in file
+# /usr/share/common-licenses/AGPL-3; if not, see
+# <http://www.gnu.org/licenses/>.
+
 import dns.resolver
 import netifaces
 import os
@@ -60,6 +91,7 @@ def get_ucs_domainname_of_dns_server():
 
 @execute_as_root
 def get_nameservers_set_via_networkmanager():
+	# TODO: Probably doesn't work for too many distributions. Check distribution first.
 	nm_config_dir = '/etc/NetworkManager/system-connections/'
 	nm_config_files = os.listdir(nm_config_dir)
 	nameservers = set()
